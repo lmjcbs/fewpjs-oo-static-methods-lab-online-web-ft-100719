44060f2790db09076baefb77c9b823f7dc3658db
lmjcbs <hello@liamjacobs.dev>
2020-02-06 13:02:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  
+  static sanitize(string) {
+    return string.split(' ').map(word => word.replace(/[^A-Za-z0-9-']+/g, '')).join(' ')
+  }
+
+  static titleize(string) {
+    const protectedWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    string = Formatter.capitalize(string)
+    return string.split(' ').map(word => {
+      if (protectedWords.includes(word)) {
+        return word
+      } else {
+        return Formatter.capitalize(word)
+      }
+    }).join(' ')
+  }
 }
